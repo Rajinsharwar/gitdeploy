12ae7de86fe880a525b765d283ca4427de680f97
rajinsharwar <rajinsharwar@gmail.com>
2024-09-14 19:28:38 -0400

    Using more optimized way to pull

--- admin/classes/background-async-pull/class-bg-async-pull.php
+++ admin/classes/background-async-pull/class-bg-async-pull.php
@@ -13,61 +13,135 @@ class WP_GitDeploy_Async extends WP_Async_Request {
 	 * Override this method to perform any actions required
 	 * during the async request.
 	 */
-	protected function handle() {
+    protected function handle() {
         $creds = get_option( 'wp_gitdeploy_creds', array() );
-
+    
         $username = $creds[ 'wp_gitdeploy_username' ] ?? '';
         $token = $creds[ 'wp_gitdeploy_token' ] ?? '';
         $repo = $creds[ 'wp_gitdeploy_repo' ] ?? '';
         $branch = $creds[ 'wp_gitdeploy_repo_branch' ] ?? 'main';
-
+    
         $changed_files = $_POST[ 'changed_files' ];
+    
+        $status = 'Success'; // Default status is success.
+    
+        // Fetch the entire repository recursively starting from the root
+        $this->process_directory( $username, $repo, $branch, '', $changed_files, $token, $status );
+    
+        $deployment_log = new \WP_GitDeploy_Deployments( $status, json_encode( $changed_files ) );
+    }
 
-        $status = 'Success'; // The default status is success.
-
-        foreach ($changed_files as $file) {
-            $url = "https://api.github.com/repos/$username/$repo/contents/$file?ref=$branch";
+    /**
+     * Method to Process the Content Directory
+     */
+    protected function process_directory( $username, $repo, $branch, $directory, $changed_files, $token, &$status ) {
+        // Fetch the contents of the current directory ('' for the root)
+        $url = "https://api.github.com/repos/$username/$repo/contents/$directory?ref=$branch";
     
-            $response = wp_remote_get($url, [
-                'headers' => [
-                    'Accept' => 'application/vnd.github+json',
-                    'Authorization' => 'Bearer ' . $token,
-                    'X-GitHub-Api-Version' => '2022-11-28',
-                ],
-            ]);
+        $response = wp_remote_get($url, [
+            'headers' => [
+                'Accept' => 'application/vnd.github+json',
+                'Authorization' => 'Bearer ' . $token,
+                'X-GitHub-Api-Version' => '2022-11-28',
+            ],
+        ]);
     
-            if ( is_wp_error( $response ) ) {
-                $status = 'Failed';
-                continue; // Handle the error as needed
-            }
+        if ( is_wp_error( $response ) ) {
+            $status = 'Failed';
+            return;
+        }
     
-            $body = wp_remote_retrieve_body($response);
-            $file_data = json_decode($body, true);
-            $upload_dir = WP_CONTENT_DIR . '/';
-            $file_path = $upload_dir . $file;
+        $body = wp_remote_retrieve_body( $response );
+        $directory_contents = json_decode( $body, true );
     
-            if (isset($file_data['content']) && $file_data['encoding'] == 'base64') {
-                $file_content = base64_decode($file_data['content']);
+        if ( ! is_array( $directory_contents ) ) {
+            $status = 'Failed';
+            return;
+        }
     
-                // Ensure the directory exists
-                if (!file_exists(dirname($file_path))) {
-                    mkdir(dirname($file_path), 0755, true);
+        // Extract paths of files and directories in the repo
+        $repo_files = array_map( function ( $item ) {
+            return $item[ 'path' ];
+        }, $directory_contents );
+    
+        // Handle deletion of local files that no longer exist in the repo
+        foreach ( $changed_files as $changed_file ) {
+            if ( strpos( $changed_file, $directory ) === 0 ) {
+                if ( ! in_array( $changed_file, $repo_files ) ) {
+                    $this->delete_file( $changed_file, $status );
                 }
+            }
+        }
     
-                // Write the file content
-                file_put_contents($file_path, $file_content);
-            } elseif ( 404 == $file_data['status'] ) {
-                // Handle file or folder deletion
-                if (file_exists($file_path)) {
-                    unlink($file_path);
-                    // Check if the directory is empty and delete it if so
-                    $this->delete_empty_directories( dirname( $file_path ) );
+        // Process each item in the current directory
+        foreach ( $directory_contents as $item ) {
+            $item_path = $item[ 'path' ];
+    
+            if ( $item[ 'type' ] === 'dir' ) {
+                // Recursively process subdirectories
+                $this->process_directory( $username, $repo, $branch, $item_path, $changed_files, $token, $status );
+            } elseif ( $item[ 'type' ] === 'file' ) {
+                // Download and save changed files
+                if ( in_array( $item_path, $changed_files ) ) {
+                    $this->download_and_save_file( $item, $changed_files, $status );
                 }
             }
         }
 
-        $deployment_log = new \WP_GitDeploy_Deployments( $status, json_encode( $changed_files ) );
-	}
+        // Handle deletion of empty directories
+        $upload_dir = WP_CONTENT_DIR . '/';
+
+        foreach ( $changed_files as $changed_file ) {
+            if ( strpos( $changed_file, $directory ) === 0) {
+                if ( ! in_array( $changed_file, $repo_files ) ) {
+                    $local_file_path = $upload_dir . $changed_file;
+                    $this->delete_empty_directories( dirname( $local_file_path ) );
+                }
+            }
+        }
+    }
+    
+    /**
+     * Method to Download and Save changed files.
+     */
+    protected function download_and_save_file( $file_data, $changed_files, &$status ) {
+        $file_url = $file_data[ 'download_url' ];
+    
+        // Fetch the file content
+        $file_content = wp_remote_get( $file_url );
+    
+        if ( is_wp_error( $file_content ) ) {
+            $status = 'Failed';
+            return;
+        }
+    
+        $decoded_content = wp_remote_retrieve_body( $file_content );
+    
+        $upload_dir = WP_CONTENT_DIR . '/';
+        $local_file_path = $upload_dir . $file_data[ 'path' ];
+    
+        // Ensure the directory exists
+        if ( ! file_exists( dirname( $local_file_path ) ) ) {
+            mkdir( dirname( $local_file_path ), 0755, true );
+        }
+    
+        // Write the file content
+        file_put_contents( $local_file_path, $decoded_content );
+    }
+
+    /**
+     * Delete the files that doesn't exist in repo.
+     */
+    protected function delete_file( $file_path, &$status ) {
+        $upload_dir = WP_CONTENT_DIR . '/';
+        $local_file_path = $upload_dir . $file_path;
+    
+        if ( file_exists( $local_file_path ) ) {
+            if ( ! unlink( $local_file_path ) ) {
+                $status = 'Failed';
+            }
+        }
+    }
 
     /**
      * Helper function to delete empty directories.
@@ -75,12 +149,15 @@ class WP_GitDeploy_Async extends WP_Async_Request {
      * @param string $dir Directory path.
      * @return void
      */
-    private function delete_empty_directories( $dir ) {
+    protected function delete_empty_directories( $dir ) {
+        // Check if the directory exists
         if ( is_dir( $dir ) ) {
-            $items = array_diff( scandir( $dir ), [ '.', '..' ] ); // Get items excluding . and ..
-            if ( empty( $items ) ) {
-                rmdir( $dir ); // Delete the directory if empty
-                $this->delete_empty_directories( dirname( $dir ) ); // Recursively check and delete parent directories if empty
+            // Check if the directory is empty
+            $files = scandir( $dir );
+            if ( count( $files ) == 2 ) { // "." and ".." are always present in a directory
+                if ( rmdir( $dir ) ) {
+                    $this->delete_empty_directories( dirname( $dir ) );
+                }
             }
         }
     }
